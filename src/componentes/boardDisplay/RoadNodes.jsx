b7140f5982f00fb0702c0520ba7c88d1a5d5a804
Doug Hirth <dmh838@hotmail.com>
2025-08-25 18:14:58 -0700

    Start of Preventing roads to be built through opponent settlements
    this is still a work in progress, but need to switch computers

--- src/componentes/boardDisplay/RoadNodes.jsx
+++ src/componentes/boardDisplay/RoadNodes.jsx
@@ -23,12 +23,20 @@ export default function RoadNodes(props) {
           let drawLine = false;
           if( props.tileCornerNodes[x][y].rightRoadOwner == "none" && (
                 props.tileCornerNodes[x][y].owner == props.currentPlayer ||
-                props.tileCornerNodes[x+1][y].owner == props.currentPlayer ||
-                props.tileCornerNodes[x-1][y].rightRoadOwner == props.currentPlayer ||
-                props.tileCornerNodes[x+1][y].rightRoadOwner == props.currentPlayer ||
+                props.tileCornerNodes[x+1][y].owner == props.currentPlayer || (
+                  props.tileCornerNodes[x-1][y].rightRoadOwner == props.currentPlayer && (
+                    props.tileCornerNodes[x][y].owner == "none" || 
+                    props.tileCornerNodes[x][y].owner == props.currentPlayer)) || (
+                  props.tileCornerNodes[x+1][y].rightRoadOwner == props.currentPlayer && (
+                    props.tileCornerNodes[x+1][y].owner == "none" || 
+                    props.tileCornerNodes[x+1][y].owner == props.currentPlayer)) ||
                 (x+y)%2 == 0 &&(
-                  props.tileCornerNodes[x][y].bottomRoadOwner == props.currentPlayer ||
-                  props.tileCornerNodes[x+1][y-1].bottomRoadOwner == props.currentPlayer) ||
+                  props.tileCornerNodes[x][y].bottomRoadOwner == props.currentPlayer && (
+                    props.tileCornerNodes[x][y].owner == "none" || 
+                    props.tileCornerNodes[x][y].owner == props.currentPlayer) ||
+                  props.tileCornerNodes[x+1][y-1].bottomRoadOwner == props.currentPlayer && (
+                    props.tileCornerNodes[x-1][y].owner == "none" || 
+                    props.tileCornerNodes[x-1][y].owner == props.currentPlayer)) || //Working on this stuff right aroun here, things aren't right
                 (x+y)%2 == 1 &&(
                   props.tileCornerNodes[x+1][y].bottomRoadOwner == props.currentPlayer ||
                   props.tileCornerNodes[x][y-1].bottomRoadOwner == props.currentPlayer))
