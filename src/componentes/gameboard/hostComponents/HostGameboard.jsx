98dfad6c3d12889819e022e315e5b57ac08f4fa8
Doug Hirth <dmh838@hotmail.com>
2025-10-07 06:42:26 -0700

    Updated Roads

--- src/componentes/gameboard/hostComponents/HostGameboard.jsx
+++ src/componentes/gameboard/hostComponents/HostGameboard.jsx
@@ -7,29 +7,86 @@ import HostTileNumbers from "./HostTileNumbers.jsx";
 import HostPorts from "./HostPorts.jsx";
 import HostBanditIcon from "./HostBanditIcon.jsx";
 
+import rightRoadSprite from './assets/rightRoadSprite.png'
+import verticalRoadSprite from './assets/verticalRoadSprite.png'
+
+import rightRoadBlue from   './assets/roads/rightRoadSprite-blue.png'
+import rightRoadRed from    './assets/roads/rightRoadSprite-red.png'
+import rightRoadIndigo from './assets/roads/rightRoadSprite-indigo.png'
+import rightRoadYellow from './assets/roads/rightRoadSprite-yellow.png'
+import rightRoadOrange from './assets/roads/rightRoadSprite-orange.png'
+import rightRoadGreen from  './assets/roads/rightRoadSprite-green.png'
+
+import downRoadBlue from   './assets/roads/verticalRoadSprite-blue.png'
+import downRoadRed from    './assets/roads/verticalRoadSprite-red.png'
+import downRoadIndigo from './assets/roads/verticalRoadSprite-indigo.png'
+import downRoadYellow from './assets/roads/verticalRoadSprite-yellow.png'
+import downRoadOrange from './assets/roads/verticalRoadSprite-orange.png'
+import downRoadGreen from  './assets/roads/verticalRoadSprite-green.png'
+
+
+
 export default function HostGameboard() {
+  const  roadSprites = {
+    Blue:  {right:rightRoadBlue, down: downRoadBlue},
+    Red:   {right:rightRoadRed, down: downRoadRed},
+    Indigo:{right:rightRoadIndigo, down: downRoadIndigo},
+    Yellow:{right:rightRoadYellow, down: downRoadYellow},
+    Orange:{right:rightRoadOrange, down: downRoadOrange},
+    Green: {right:rightRoadGreen, down: downRoadGreen}
+  }
+
+  const rightRoadOffset = 
+  [
+    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
+    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:150, y:305},{x:115, y:450},{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
+    [{x:0,y:0},{x:0,y:0}    ,{x:335, y:165},{x:310, y:305},{x:275, y:450},{x:250, y:590},{x:0,y:0}     ,{x:0,y:0}],
+    [{x:0,y:0},{x:500, y:25},{x:485, y:165},{x:460, y:305},{x:440, y:450},{x:420, y:590},{x:400, y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:645, y:25},{x:630, y:165},{x:615, y:305},{x:600, y:450},{x:590, y:590},{x:575, y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:780, y:25},{x:775, y:165},{x:770, y:305},{x:765, y:450},{x:760, y:590},{x:760, y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:920, y:25},{x:920, y:165},{x:925, y:305},{x:930, y:450},{x:935, y:590},{x:935, y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:1050,y:25},{x:1065,y:165},{x:1080,y:305},{x:1095,y:450},{x:1105,y:590},{x:1115,y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:1195,y:25},{x:1210,y:165},{x:1235,y:305},{x:1260,y:450},{x:1275,y:590},{x:1295,y:735},{x:0,y:0}],
+    [{x:0,y:0},{x:0,y:0}    ,{x:1360,y:165},{x:1390,y:305},{x:1420,y:450},{x:1445,y:590},{x:0,y:0}     ,{x:0,y:0}],
+    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:1540,y:305},{x:1580,y:450},{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
+    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}]
+  ];
+  const downRoadOffset = 
+  [
+    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:0,   y:0}  ,{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:60,  y:375},{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:255 ,y:235},{x:0   ,y:0}  ,{x:185 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:430,y:95} ,{x:0   ,y:0}  ,{x:380 ,y:375},{x:0   ,y:0}  ,{x:330 ,y:660},{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:555 ,y:235},{x:0   ,y:0}  ,{x:520 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:715 ,y:95},{x:0   ,y:0}  ,{x:695 ,y:375},{x:0   ,y:0}  ,{x:680 ,y:660},{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:855 ,y:235},{x:0   ,y:0}  ,{x:855 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:1000,y:95},{x:0   ,y:0}  ,{x:1015,y:375},{x:0   ,y:0}  ,{x:1035,y:660},{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:1160,y:235},{x:0   ,y:0}  ,{x:1190,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:1280,y:95},{x:0   ,y:0}  ,{x:1335,y:375},{x:0   ,y:0}  ,{x:1385,y:660},{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:1458,y:235},{x:0,   y:0}  ,{x:1525,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
+    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:1650,y:375},{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}]
+  ];
 
   return(
     <svg viewBox="0 0 1920 1080">
       <image href={backgroundMap} width={'1920'} height={'1080'} />
       <g>
         <HostPorts />
-        <HostRoads y={1} />
         <HostCities y={1} />
         <HostTiles y={1} />
-        <HostRoads y={2} />
+        <HostRoads y={2} rightRoadSprite={rightRoadSprite} rightRoadOffset={rightRoadOffset} verticalRoadSprite={verticalRoadSprite} downRoadOffset={downRoadOffset} roadSprites={roadSprites} />
         <HostCities y={2} />
         <HostTiles y={2} />
-        <HostRoads y={3} />
+        <HostRoads y={3} rightRoadSprite={rightRoadSprite} rightRoadOffset={rightRoadOffset} verticalRoadSprite={verticalRoadSprite} downRoadOffset={downRoadOffset} roadSprites={roadSprites} />
         <HostCities y={3} />
         <HostTiles y={3} />
-        <HostRoads y={4} />
+        <HostRoads y={4} rightRoadSprite={rightRoadSprite} rightRoadOffset={rightRoadOffset} verticalRoadSprite={verticalRoadSprite} downRoadOffset={downRoadOffset} roadSprites={roadSprites} />
         <HostCities y={4} />
         <HostTiles y={4} />
-        <HostRoads y={5} />
+        <HostRoads y={5} rightRoadSprite={rightRoadSprite} rightRoadOffset={rightRoadOffset} verticalRoadSprite={verticalRoadSprite} downRoadOffset={downRoadOffset} roadSprites={roadSprites} />
         <HostCities y={5} />
         <HostTiles y={5} />
-        <HostRoads y={6} />
+        <HostRoads y={6} rightRoadSprite={rightRoadSprite} rightRoadOffset={rightRoadOffset} verticalRoadSprite={verticalRoadSprite} downRoadOffset={downRoadOffset} roadSprites={roadSprites} />
         <HostCities y={6} />
         <HostTileNumbers />
         <HostBanditIcon />
--- src/componentes/gameboard/hostComponents/HostRoads.jsx
+++ src/componentes/gameboard/hostComponents/HostRoads.jsx
@@ -2,42 +2,9 @@ import { useContext } from "react";
 import { TileCornerNodesContext } from "../state/tileCornerNodes/TileCornerNodesContext";
 import { PlayerInformationContext } from "../../../state/playerInformation/PlayerInformationContext";
 
-import HostRoad from "./HostRoad";
-
 export default function HostRoads( props ) {
   const { tileCornerNodes } = useContext(TileCornerNodesContext);
   const { playerColor } = useContext(PlayerInformationContext);
-
-  const rightRoadOffset = 
-  [
-    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
-    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:150, y:310},{x:115, y:455},{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
-    [{x:0,y:0},{x:0,y:0}    ,{x:335, y:170},{x:310, y:310},{x:275, y:455},{x:250, y:595},{x:0,y:0}     ,{x:0,y:0}],
-    [{x:0,y:0},{x:500, y:30},{x:485, y:170},{x:460, y:310},{x:440, y:455},{x:420, y:595},{x:400, y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:645, y:30},{x:630, y:170},{x:615, y:310},{x:600, y:455},{x:590, y:595},{x:575, y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:780, y:30},{x:775, y:170},{x:770, y:310},{x:765, y:455},{x:760, y:595},{x:760, y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:920, y:30},{x:920, y:170},{x:925, y:310},{x:930, y:455},{x:935, y:595},{x:935, y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:1050,y:30},{x:1065,y:170},{x:1080,y:310},{x:1095,y:455},{x:1105,y:595},{x:1115,y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:1195,y:30},{x:1210,y:170},{x:1235,y:310},{x:1260,y:455},{x:1275,y:595},{x:1295,y:740},{x:0,y:0}],
-    [{x:0,y:0},{x:0,y:0}    ,{x:1360,y:170},{x:1390,y:310},{x:1420,y:455},{x:1445,y:595},{x:0,y:0}     ,{x:0,y:0}],
-    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:1540,y:310},{x:1580,y:455},{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}],
-    [{x:0,y:0},{x:0,y:0}    ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}     ,{x:0,y:0}]
-  ];
-    const downRoadOffset = 
-  [
-    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:0,   y:0}  ,{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:60,  y:375},{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:255 ,y:235},{x:0   ,y:0}  ,{x:185 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:430,y:95} ,{x:0   ,y:0}  ,{x:380 ,y:375},{x:0   ,y:0}  ,{x:330 ,y:660},{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:555 ,y:235},{x:0   ,y:0}  ,{x:520 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:715 ,y:95},{x:0   ,y:0}  ,{x:695 ,y:375},{x:0   ,y:0}  ,{x:680 ,y:660},{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:855 ,y:235},{x:0   ,y:0}  ,{x:855 ,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:1000,y:95},{x:0   ,y:0}  ,{x:1015,y:375},{x:0   ,y:0}  ,{x:1035,y:660},{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:1160,y:235},{x:0   ,y:0}  ,{x:1190,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:1280,y:95},{x:0   ,y:0}  ,{x:1335,y:375},{x:0   ,y:0}  ,{x:1385,y:660},{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:1458,y:235},{x:0,   y:0}  ,{x:1525,y:520},{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}],
-    [{x:0,y:0},{x:0   ,y:0} ,{x:0   ,y:0}  ,{x:1650,y:375},{x:0   ,y:0}  ,{x:0   ,y:0}  ,{x:0,y:0},{x:0,y:0}]
-  ];
 
   let boardContent = [];
   //let tileNumber = 0;
@@ -45,28 +12,79 @@ export default function HostRoads( props ) {
     if ( "rightRoadOwner" in tileCornerNodes[x][props.y]) {
       if (x < 11 && tileCornerNodes[parseInt(x)+1][props.y].value != "Ocean") {
         if (Number.isInteger(tileCornerNodes[parseInt(x)][props.y].rightRoadOwner)) {
+        console.log("Player color is ", playerColor[0]);
           boardContent.push(
-            <HostRoad 
-              key={crypto.randomUUID()}
-              translateValue={"translate(" + rightRoadOffset[x][props.y].x + "," + rightRoadOffset[x][props.y].y + ")"}
-              imagePath={"rightRoad-x"+x+"-y"+props.y+"-"+playerColor[tileCornerNodes[x][props.y].rightRoadOwner].toLowerCase()}
-              width={'224'}
-              height={'80'}
-            />
+            <g>
+              <clipPath id={"cut-rightRoad-x"+x+"-y"+props.y}>
+                <rect
+                  x={props.rightRoadOffset[x][props.y].x}
+                  y={props.rightRoadOffset[x][props.y].y}
+                  width="224"
+                  height="88"
+                />
+              </clipPath>
+              <image
+                href={props.rightRoadSprite}
+                width={2240}
+                height={528}
+                x={props.rightRoadOffset[x][props.y].x-(x-1)*224}
+                y={props.rightRoadOffset[x][props.y].y-(props.y-1)*88}
+                clip-path={"url(#cut-rightRoad-x"+x+"-y"+props.y}
+              />
+              
+              <image
+                style={{"mix-blend-mode":"multiply"}}
+                href={props.roadSprites[playerColor[tileCornerNodes[x][props.y].rightRoadOwner]].right}//CHANGE PLAYER 0 TO OWNER
+                width={2240}
+                height={528}
+                x={props.rightRoadOffset[x][props.y].x-(x-1)*224}
+                y={props.rightRoadOffset[x][props.y].y-(props.y-1)*88}
+                clip-path={"url(#cut-rightRoad-x"+x+"-y"+props.y}
+              />
+            </g>
             )
         }
       }
       if ( "bottomRoadOwner" in tileCornerNodes[x][props.y]) {
         if (Number.isInteger(tileCornerNodes[parseInt(x)][props.y].bottomRoadOwner)) {
           boardContent.push(
-            <HostRoad 
-              key={crypto.randomUUID()}
-              translateValue={"translate(" + downRoadOffset[x][props.y].x + "," + downRoadOffset[x][props.y].y + ")"}
-              imagePath={"bottomRoad-x"+x+"-y"+props.y+"-"+playerColor[tileCornerNodes[x][props.y].bottomRoadOwner].toLowerCase()}
-              width={'208'}
-              height={'96'}
-            />
+            <g>
+              <rect
+                  x={props.downRoadOffset[x][props.y].x+70}
+                  y={props.downRoadOffset[x][props.y].y-10}
+                  width="60"
+                  height="120"
+                  fill="red"
+                  fillOpacity={0.0}
+                />
+              <clipPath id={"cut-downRoad-x"+x+"-y"+props.y}>
+                <rect
+                  x={props.downRoadOffset[x][props.y].x+70}
+                  y={props.downRoadOffset[x][props.y].y-10}
+                  width="60"
+                  height="120"
+                />
+              </clipPath>
+              <image
+                href={props.verticalRoadSprite}
+                width={360}
+                height={600}
+                x={props.downRoadOffset[x][props.y].x-(x-1)*30+70}
+                y={props.downRoadOffset[x][props.y].y-(props.y-1)*120-10}
+                clip-path={"url(#cut-downRoad-x"+x+"-y"+props.y}
+              />
+              <image
+                style={{"mix-blend-mode":"multiply"}}
+                href={props.roadSprites[playerColor[tileCornerNodes[x][props.y].bottomRoadOwner]].down}//CHANGE PLAYER 0 TO OWNER
+                width={360}
+                height={600}
+                x={props.downRoadOffset[x][props.y].x-(x-1)*30+70}
+                y={props.downRoadOffset[x][props.y].y-(props.y-1)*120-10}
+                clip-path={"url(#cut-downRoad-x"+x+"-y"+props.y}
+              />
+            </g>
           )
+          
         }
       }
     }
